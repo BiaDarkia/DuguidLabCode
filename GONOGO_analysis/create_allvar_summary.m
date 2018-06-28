d2525c81d12d22b9d668195eed412f18930ccf17
FedeClaudi <federicoclaudi@gmail.com>
2018-06-28 12:34:28 +0100

    Now saving a summary version of all var

---
+++ GONOGO_analysis/create_allvar_summary.m
@@ -0,0 +1,69 @@
+% Mouse details
+summary_allvar.mouse_name = all_var.mouse_name;
+
+% Number of rewards
+summary_allvar.num_rewards_Go = all_var.num_rewards_Go ;
+summary_allvar.num_rewards_Nogo = all_var.num_rewards_Nogo;
+% Number of Tonoes
+summary_allvar.num_tones_Go   = all_var.num_tones_Go  ;  
+summary_allvar.num_tones_Nogo = all_var.num_tones_Nogo  ;  
+summary_allvar.num_trials   = all_var.num_trials   ;  
+% Number of erroneus trials
+summary_allvar.misses     = all_var.misses          ;  
+summary_allvar.false_alarm  = all_var.false_alarm   ;   
+% Preemptives
+summary_allvar.num_preemptive_ITI = all_var.num_preemptive_ITI ;               
+summary_allvar.num_preemptive_ITI_Complete = all_var.num_preemptive_ITI_Complete;     
+summary_allvar.num_preemptive_ITI_Incomplete = all_var.num_preemptive_ITI_Incomplete;    
+summary_allvar.complete_preemptive_ITI_ratio = all_var.complete_preemptive_ITI_ratio;    
+% Tone efficiency
+summary_allvar.tone_efficiency_Go      = all_var.tone_efficiency_Go     ;    
+summary_allvar.tone_efficiency_Nogo    = all_var.tone_efficiency_Nogo     ;   
+summary_allvar.tone_efficiency_tot     = all_var.tone_efficiency_tot     ;   
+
+summary_allvar.reaction_time_Hit_avg   = all_var.reaction_time_Hit_avg     ;   
+summary_allvar.reaction_time_FalseAlarm_avg = all_var.reaction_time_FalseAlarm_avg;   
+
+%Discr index and Bias index 
+summary_allvar.DiscrIndx       = all_var.DiscrIndx              ;   
+summary_allvar.BiasIndx        = all_var.BiasIndx             ;   
+
+% Opto stuff
+summary_allvar.Hit_OptoStim  = all_var.Hit_OptoStim               ;           
+summary_allvar.Hit_NoStim    = all_var.Hit_NoStim               ;           
+summary_allvar.Miss_OptoStim = all_var.Miss_OptoStim               ;           
+summary_allvar.Miss_NoStim   = all_var.Miss_NoStim               ;           
+summary_allvar.FalseAlarm_OptoStim = all_var.FalseAlarm_OptoStim         ;           
+summary_allvar.FalseAlarm_NoStim   = all_var.FalseAlarm_NoStim         ;           
+summary_allvar.CorrectRejection_OptoStim = all_var.CorrectRejection_OptoStim   ;           
+summary_allvar.CorrectRejection_NoStim  = all_var.CorrectRejection_NoStim     ;          
+summary_allvar.Preemptives_ITI_OptoStim = all_var.Preemptives_ITI_OptoStim     ;          
+summary_allvar.Preemptives_ITI_OptoStim_Complete = all_var.Preemptives_ITI_OptoStim_Complete    ;   
+summary_allvar.Preemptives_ITI_OptoStim_Incomplete = all_var.Preemptives_ITI_OptoStim_Incomplete ;   
+summary_allvar.Preemptives_ITI_NoStim  = all_var.Preemptives_ITI_NoStim             ;   
+summary_allvar.Preemptives_ITI_NoStim_Complete  = all_var.Preemptives_ITI_NoStim_Complete    ;   
+summary_allvar.Preemptives_ITI_NoStim_Incomplete = all_var.Preemptives_ITI_NoStim_Incomplete   ;   
+
+summary_allvar.reaction_time_Hit_median_OptoStim    = all_var.reaction_time_Hit_median_OptoStim       ;
+summary_allvar.reaction_time_FalseAlarm_median_OptoStim  = all_var.reaction_time_FalseAlarm_median_OptoStim  ;
+summary_allvar.reaction_time_Hit_std_OptoStim   = all_var.reaction_time_Hit_std_OptoStim           ;
+summary_allvar.reaction_time_FalseAlarm_std_OptoStim   = all_var.reaction_time_FalseAlarm_std_OptoStim    ;
+ 
+summary_allvar.reaction_time_Hit_median_NoStim    = all_var.reaction_time_Hit_median_NoStim         ;
+summary_allvar.reaction_time_FalseAlarm_median_NoStim = all_var.reaction_time_FalseAlarm_median_NoStim     ;
+summary_allvar.reaction_time_Hit_std_NoStim  = all_var.reaction_time_Hit_std_NoStim              ;
+summary_allvar.reaction_time_FalseAlarm_std_NoStim  = all_var.reaction_time_FalseAlarm_std_NoStim       ;
+
+summary_allvar.tone_efficiency_Go_OptoStim   = all_var.tone_efficiency_Go_OptoStim              ;
+summary_allvar.tone_efficiency_Nogo_OptoStim  = all_var.tone_efficiency_Nogo_OptoStim             ;
+
+summary_allvar.tone_efficiency_Go_NoStim   = all_var.tone_efficiency_Go_NoStim                ;
+summary_allvar.tone_efficiency_Nogo_NoStim  = all_var.tone_efficiency_Nogo_NoStim               ;
+
+summary_allvar.complete_preemptive_ITI_ratio_OptoStim  = all_var.complete_preemptive_ITI_ratio_OptoStim    ;
+summary_allvar.complete_preemptive_ITI_ratio_NoStim    = all_var.complete_preemptive_ITI_ratio_NOStim    ;
+
+summary_allvar.DiscrIndx_OptoStim    = all_var.DiscrIndx_OptoStim                      ;
+summary_allvar.BiasIndx_OptoStim     = all_var.BiasIndx_OptoStim                      ;
+summary_allvar.DiscrIndx_NoStim      = all_var.DiscrIndx_NoStim                      ;
+summary_allvar.BiasIndx_NoStim       = all_var.BiasIndx_NoStim                       ;
